7b1a7b7e2b3ccb8822c6b72422417204841ab519
박민한 <runningwater@optatumplatform.com>
2021-03-26 17:51:43 +0900

    add react-slick

---
+++ pages/search.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Search = () => {
+  let setting = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+  };
+  return (
+    <div style={{ width: "100%", padding: "4px" }}>
+      <Slider {...setting}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Search;
